80af96544f89876a2ebe1dca41f4813be8d45c89
Robson Inocêncio <robson.inocencio@gmail.com>
2025-05-28 17:23:59 -0300

    ajuste

--- app/homeprisma/page.tsx
+++ app/homeprisma/page.tsx
@@ -7,6 +7,7 @@ export default async function Home() {
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
         Superblog
       </h1>
+
       <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
         {users.map((user) => (
           <li key={user.id} className="mb-2">
