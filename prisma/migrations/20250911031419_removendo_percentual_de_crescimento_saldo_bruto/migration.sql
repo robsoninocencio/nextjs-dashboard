1baa25e54e1f5b4c9c506c0c6f9aa70c6097e8e8
Robson Inocêncio <robson.inocencio@gmail.com>
2025-09-11 01:03:09 -0300

    removendo percentualDeCrescimentoSaldoBruto

---
+++ prisma/migrations/20250911031419_removendo_percentual_de_crescimento_saldo_bruto/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `percentualDeCrescimentoSaldoBruto` on the `investimentos` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."investimentos" DROP COLUMN "percentualDeCrescimentoSaldoBruto";
