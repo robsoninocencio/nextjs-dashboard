075cff03a2960e4463e15af7a6253fef91a69475
Robson Inocêncio <robson.inocencio@gmail.com>
2025-10-14 14:49:54 -0300

    ajustando grafico

--- app/ui/investimentos/performance-chart.tsx
+++ app/ui/investimentos/performance-chart.tsx
@@ -51,7 +51,7 @@ export function PerformanceChart({ data }: PerformanceChartProps) {
       <CardContent>
         <div className='h-80'>
           <ResponsiveContainer width='100%' height='100%'>
-            <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <LineChart data={data} margin={{ top: 5, right: 30, left: 50, bottom: 5 }}>
               <CartesianGrid strokeDasharray='3 3' />
               <XAxis
                 dataKey='periodo'
